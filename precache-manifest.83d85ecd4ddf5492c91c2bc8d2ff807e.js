a4f6d9ea12a290a400e74a83ca2e397bf8dd7e26
saelani <saelani@gmail.com>
2020-06-25 23:04:08 +0700

    deploy

---
+++ precache-manifest.83d85ecd4ddf5492c91c2bc8d2ff807e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b62baa99a68db6d6ec8",
+    "url": "/static/js/main.6b62baa9.chunk.js"
+  },
+  {
+    "revision": "2d03a5e01dd99ebe403f",
+    "url": "/static/js/1.2d03a5e0.chunk.js"
+  },
+  {
+    "revision": "6b62baa99a68db6d6ec8",
+    "url": "/static/css/main.f0d3e61a.chunk.css"
+  },
+  {
+    "revision": "7383150a1d191a4707871e6cc4b98ff8",
+    "url": "/index.html"
+  }
+];
